677bccb4dec05af4e901e80d2678cf7e11a1bd56
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-07-14 15:56:23 +0300

    chore: replace __dirname alias with fileURLToPath in vite.config.js to fix ESM import errors

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
+// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      // замість path.resolve(__dirname, 'src')
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
