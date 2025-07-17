27d0e77f58f8ac73fcb93bd8f37a5fc9550bed20
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-07-17 18:08:53 +0300

    feat: add routes for features and reviews section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import SharedLayout from "./components/SharedLayout";
 import { lazy } from "react";
 import withSuspense from "./utils/withSuspense";
+import ProductFeatures from "./components/ProductFeatures/ProductFeatures";
+import ProductReviews from "./components/ProductReviews/ProductReviews";
 
 const HomePage = withSuspense(lazy(() => import("./pages/HomePage")));
 const CatalogPage = withSuspense(lazy(() => import("./pages/CatalogPage")));
-const CamperDetailPage = withSuspense(
-  lazy(() => import("./pages/CamperDetailPage"))
+const ProductDetailsPage = withSuspense(
+  lazy(() => import("./pages/ProductDetailsPage"))
 );
 const NotFoundPage = withSuspense(lazy(() => import("./pages/NotFoundPage")));
 
@@ -17,7 +19,11 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
           <Route path="catalog" element={<CatalogPage />} />
-          <Route path="catalog/:id" element={<CamperDetailPage />} />
+          <Route path="catalog/:id" element={<ProductDetailsPage />}>
+            <Route index element={<Navigate to={"features"} replace />} />
+            <Route path={"features"} element={<ProductFeatures />} />
+            <Route path={"reviews"} element={<ProductReviews />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
