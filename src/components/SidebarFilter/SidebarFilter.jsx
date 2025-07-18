493d66ce8f69430129b0d27e24089ebfb899c7c1
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-07-18 12:17:07 +0300

    feat: implement controlled form in SidebarFilter with search and reset logic

--- src/components/SidebarFilter/SidebarFilter.jsx
+++ src/components/SidebarFilter/SidebarFilter.jsx
@@ -1,23 +1,19 @@
 import {
   Box,
-  TextField,
-  FormControl,
-  FormLabel,
-  FormGroup,
-  FormControlLabel,
-  Checkbox,
-  RadioGroup,
-  Radio,
-  Button,
-  Paper,
   Stack,
-  Typography,
+  TextField,
+  Button,
+  Checkbox,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormGroup,
+  FormLabel,
 } from "@mui/material";
-import AppIcon from "@/components/common/AppIcon/AppIcon";
-import { selectFilter } from "@/redux/products/productsSelectors";
-import { resetFilter, setFilter } from "@/redux/products/productsSlice";
-import { useDispatch, useSelector } from "react-redux";
-import IconTile from "../common/IconTile/IconTile";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { setFilter, resetFilter } from "@/redux/products/productsSlice";
+import IconTile from "@/components/common/IconTile/IconTile";
 
 const productTypes = ["alcove", "fullyIntegrated", "panelTruck"];
 const productOptions = [
@@ -32,144 +28,112 @@ const productOptions = [
   "water",
 ];
 
+const initialFilter = {
+  location: "",
+  options: [],
+  type: null,
+};
+
 const SidebarFilter = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(selectFilter);
+  const [localFilter, setLocalFilter] = useState(initialFilter);
 
   const handleLocation = (e) => {
-    dispatch(setFilter({ location: e.target.value || null }));
+    setLocalFilter((prev) => ({ ...prev, location: e.target.value }));
   };
 
-  const handleOptions = (e) => {
-    const value = e.target.value;
-    const checked = e.target.checked;
-
-    const updated = checked
-      ? [...filter.options, value]
-      : filter.options.filter((item) => item !== value);
-
-    dispatch(setFilter({ options: updated }));
+  const handleOptionChange = (e) => {
+    const { value, checked } = e.target;
+    setLocalFilter((prev) => ({
+      ...prev,
+      options: checked
+        ? [...prev.options, value]
+        : prev.options.filter((opt) => opt !== value),
+    }));
   };
 
-  const handleType = (e) => {
-    dispatch(setFilter({ type: e.target.value || null }));
+  const handleTypeChange = (e) => {
+    setLocalFilter((prev) => ({
+      ...prev,
+      type: e.target.value || null,
+    }));
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    dispatch(setFilter({ ...localFilter, page: 1 }));
   };
 
   const handleReset = () => {
+    setLocalFilter(initialFilter);
     dispatch(resetFilter());
   };
 
   return (
     <Box component="aside">
-      <Stack spacing={4}>
-        {/* Location input */}
-        <TextField
-          label="Location"
-          variant="outlined"
-          size="small"
-          fullWidth
-          value={filter.location || ""}
-          onChange={handleLocation}
-        />
+      <Box component="form" onSubmit={handleSearch}>
+        <Stack spacing={4}>
+          {/* Location */}
+          <TextField
+            label="Location"
+            name="location"
+            variant="outlined"
+            size="small"
+            fullWidth
+            value={localFilter.location}
+            onChange={handleLocation}
+          />
 
-        {/* Vehicle equipment */}
-        <FormControl component="fieldset">
-          <FormLabel>Vehicle equipment</FormLabel>
-          <FormGroup row>
-            {productOptions.map((opt) => (
-              <Checkbox
-                key={opt}
-                checked={filter.options.includes(opt)}
-                onChange={handleOptions}
-                value={opt}
-                icon={<IconTile label={opt} name={opt} />}
-                checkedIcon={<IconTile label={opt} name={opt} active />}
-              />
-            ))}
-          </FormGroup>
-        </FormControl>
+          {/* Vehicle equipment */}
+          <FormControl component="fieldset">
+            <FormLabel>Vehicle equipment</FormLabel>
+            <FormGroup row>
+              {productOptions.map((opt) => (
+                <Checkbox
+                  key={opt}
+                  name="options"
+                  value={opt}
+                  checked={localFilter.options.includes(opt)}
+                  onChange={handleOptionChange}
+                  icon={<IconTile label={opt} name={opt} />}
+                  checkedIcon={<IconTile label={opt} name={opt} active />}
+                />
+              ))}
+            </FormGroup>
+          </FormControl>
 
-        {/* Vehicle type */}
-        <FormControl component="fieldset">
-          <FormLabel>Vehicle type</FormLabel>
-          <RadioGroup row value={filter.type ?? ""} onChange={handleType}>
-            {/* <FormControlLabel value="" control={<Radio />} label="All" /> */}
-            {productTypes.map((type) => (
-              <Radio
-                key={type}
-                value={type}
-                icon={<IconTile label={type} name={type} />}
-                checkedIcon={<IconTile label={type} name={type} active />}
-              />
-            ))}
-          </RadioGroup>
-        </FormControl>
+          {/* Vehicle type */}
+          <FormControl component="fieldset">
+            <FormLabel>Vehicle type</FormLabel>
+            <RadioGroup
+              row
+              name="type"
+              value={localFilter.type ?? ""}
+              onChange={handleTypeChange}
+            >
+              {productTypes.map((type) => (
+                <Radio
+                  key={type}
+                  value={type}
+                  icon={<IconTile label={type} name={type} />}
+                  checkedIcon={<IconTile label={type} name={type} active />}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
 
-        {/* Reset button */}
-        <Box>
-          <Button variant="contained" onClick={handleReset}>
-            Reset
-          </Button>
-        </Box>
-      </Stack>
+          {/* Buttons */}
+          <Box display="flex" gap={2}>
+            <Button type="submit" variant="contained">
+              Search
+            </Button>
+            <Button type="button" onClick={handleReset} variant="outlined">
+              Reset
+            </Button>
+          </Box>
+        </Stack>
+      </Box>
     </Box>
-
-    // <aside>
-    //   <div>
-    //     <label htmlFor="location">Location</label>
-    //     <input
-    //       id="location"
-    //       type="text"
-    //       name="location"
-    //       value={filter.location || ""}
-    //       onChange={handleLocation}
-    //     />
-    //   </div>
-
-    //   <fieldset>
-    //     <legend>Vehicle equipment</legend>
-    //     {productOptions.map((opt) => (
-    //       <label key={opt}>
-    //         <input
-    //           type="checkbox"
-    //           value={opt}
-    //           checked={filter.options.includes(opt)}
-    //           onChange={handleOptions}
-    //         />
-    //         {opt}
-    //       </label>
-    //     ))}
-    //   </fieldset>
-
-    //   <fieldset>
-    //     <legend>Vehicle type</legend>
-    //     <label>
-    //       <input
-    //         type="radio"
-    //         name="type"
-    //         value=""
-    //         checked={filter.type === null}
-    //         onChange={handleType}
-    //       />
-    //       All
-    //     </label>
-    //     {productTypes.map((type) => (
-    //       <label key={type}>
-    //         <input
-    //           type="radio"
-    //           name="type"
-    //           value={type}
-    //           checked={type === filter.type}
-    //           onChange={handleType}
-    //         />
-    //         {type}
-    //       </label>
-    //     ))}
-    //   </fieldset>
-    //   <button type="button" onClick={handleReset}>
-    //     Reset
-    //   </button>
-    // </aside>
   );
 };
 
