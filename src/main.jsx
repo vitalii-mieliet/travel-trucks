fef8239f7ed2dc7692148aff67ad02b14a7b30e6
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-07-18 16:15:26 +0300

    feat: configure MUI theme

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,18 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "@/redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "@/theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <CssBaseline />
-          <App />
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
