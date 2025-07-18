f6d3507cbbfc5c3a6d6391e40786d1bf1cef5354
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-07-18 12:18:45 +0300

    feat: update CatalogPage layout using MUI components

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -6,6 +6,7 @@ import {
   selectProducts,
 } from "@/redux/products/productsSelectors";
 import { setFilter } from "@/redux/products/productsSlice";
+import { Box, Button, Container, Grid } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -23,15 +24,35 @@ const CatalogPage = () => {
   // console.log(productList);
 
   return (
-    <>
-      <div style={{ display: "flex" }}>
-        <SidebarFilter />
-        <ProductList products={productList} />
-      </div>
-      <button type="button" onClick={handleLoadMoreBtn}>
-        Load more
-      </button>
-    </>
+    // <>
+    //   <div style={{ display: "flex" }}>
+    //     <SidebarFilter />
+    //     <ProductList products={productList} />
+    //   </div>
+    //   <button type="button" onClick={handleLoadMoreBtn}>
+    //     Load more
+    //   </button>
+    // </>
+    <Container maxWidth="xl" sx={{ py: 4 }}>
+      <Grid container spacing={4}>
+        {/* Sidebar */}
+        <Grid size={4}>
+          <SidebarFilter />
+        </Grid>
+
+        {/* Product List */}
+        <Grid size={8}>
+          <ProductList products={productList} />
+
+          {/* Load more button */}
+          <Box textAlign="center" mt={3}>
+            <Button variant="contained" onClick={handleLoadMoreBtn}>
+              Load more
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
