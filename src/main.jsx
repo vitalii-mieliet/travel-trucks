ef63811eea34f780b45ab53dfdd39c6533531b04
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-07-18 11:27:06 +0300

    style: add css variables, CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "@/redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import { CssBaseline } from "@mui/material";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
+          <CssBaseline />
           <App />
         </BrowserRouter>
       </PersistGate>
